7477a4b62ef68d877579f1cc1048e5a699022569
Lukas Zirngibl <lukas.e.zirngibl@gmail.com>
2020-08-23 09:11:28 +0200

    add example file

---
+++ example/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { Button } from '../src/components'
+import { configureLanguage, Language } from '../src/theme/language'
+import { configureTheme, getTheme as getAppTheme } from '../src/theme/theme'
+
+export const AppThemeContext = configureTheme({
+  button: {
+    chromeless: {},
+    secondary: {},
+    common: {
+      backgroundColor: 'red',
+      color: 'white',
+    },
+  },
+})
+
+export const Translations = {
+  submit: {
+    [Language.EN]: 'Submit',
+    [Language.DE]: 'Einreichen',
+    [Language.IT]: 'Invia',
+    [Language.FR]: 'Soumettre',
+  },
+}
+
+type TM = typeof Translations
+
+const LanguageContext = configureLanguage(Translations, Language.EN)
+
+const App = () => {
+  const language = Language.EN
+
+  return (
+    <LanguageContext.Provider value={language}>
+      <AppThemeContext.Provider value={getAppTheme()}>
+        <Button<TM> label="submit" />
+      </AppThemeContext.Provider>
+    </LanguageContext.Provider>
+  )
+}
