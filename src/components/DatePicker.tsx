e723d0843785a1beb89c751193fe9cee10551b05
Lukas Zirngibl <lukas.e.zirngibl@gmail.com>
2021-02-19 11:11:38 +0100

    patch

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import DatePickerLib, { ReactDatePickerProps } from 'react-datepicker'
+import DatePickerLib, {
+  ReactDatePickerProps,
+  registerLocale,
+} from 'react-datepicker'
 import { LabelProps, Label } from './Label'
 import 'react-datepicker/dist/react-datepicker.css'
 import { TextInput } from './TextInput'
@@ -8,6 +11,26 @@ import { getThemeContext, AppTheme } from '../theme/theme'
 import { createGetStyle } from '../theme/util'
 import styled from 'styled-components'
 import ClickAwayListener from 'react-click-away-listener'
+import de from 'date-fns/locale/de'
+import en from 'date-fns/locale/en-GB'
+import fr from 'date-fns/locale/fr'
+import it from 'date-fns/locale/it'
+import { getLanguageContext } from '../theme/language'
+import { Language } from '../util'
+
+const mapLanguageToLocaleString: { [k in Language]: string } = {
+  [Language.DE]: 'de',
+  [Language.EN]: 'en-DB',
+  [Language.FR]: 'fr',
+  [Language.IT]: 'it',
+}
+
+const mapLanguageToLocale: { [k in Language]: Locale } = {
+  [Language.DE]: de,
+  [Language.EN]: en,
+  [Language.FR]: fr,
+  [Language.IT]: it,
+}
 
 const Wrapper = styled.div`
   .react-datepicker__triangle {
@@ -41,9 +64,17 @@ export const DatePicker = (props: Props) => {
 
   const theme = React.useContext(getThemeContext())
   const getStyle = createGetStyle(theme, 'datePicker')(props.style)
+  const language = React.useContext(getLanguageContext())
 
   const [open, setOpen] = React.useState(false)
   const dateFormat = props.dateFormat || 'dd.MM.yyyy'
+
+  React.useEffect(() => {
+    registerLocale(
+      mapLanguageToLocaleString[language],
+      mapLanguageToLocale[language],
+    )
+  }, [language])
 
   return (
     <>
@@ -63,6 +94,7 @@ export const DatePicker = (props: Props) => {
             }}
           />
           <DatePickerLib
+            locale={language}
             customInput={<input data-test-id={props.dataTestId} />}
             open={open}
             selected={value}
