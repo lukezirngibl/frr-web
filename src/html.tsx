aefee494278d53055f94733efe7642555ee6fd9f
Joël Meiller <>
2021-03-06 22:25:44 +0100

    Try to add css styles to html components

--- src/html.tsx
+++ src/html.tsx
@@ -2,7 +2,8 @@ import React, { ReactNode } from 'react'
 import { getThemeContext } from './theme/theme'
 import { useInlineStyle } from './theme/util'
 import { getLanguageContext, getTranslation } from './theme/language'
-import { CSSProperties } from 'styled-components'
+import { CSSProperties } from './theme/theme'
+import { createStyled } from './theme/util'
 import { Language, keys } from './util'
 
 type Props = {
@@ -13,10 +14,25 @@ type Props = {
         translate: (k: string) => string
       }) => ReactNode)
   style?: CSSProperties
+  styleCSS?: CSSProperties
   value?: any
   disabled?: any
+  readOnly?: boolean
   translationKey?: string
   data?: { [k: string]: string }
+}
+
+const Elements = {
+  p: createStyled('p'),
+  h1: createStyled('h1'),
+  h2: createStyled('h2'),
+  h3: createStyled('h3'),
+  h4: createStyled('h4'),
+  h5: createStyled('h5'),
+  h6: createStyled('h6'),
+  button: createStyled('button'),
+  li: createStyled('li'),
+  option: createStyled('option'),
 }
 
 export const Element = (
@@ -34,7 +50,14 @@ export const Element = (
       | 'option'
   },
 ) => {
-  const { label, style, element, translationKey, ...otherProps } = props
+  const {
+    label,
+    style = {},
+    readOnly,
+    element,
+    translationKey,
+    ...otherProps
+  } = props
   const theme = React.useContext(getThemeContext())
   const getStyle = useInlineStyle(theme, 'html')({})
 
@@ -53,29 +76,39 @@ export const Element = (
     str,
   )
 
-  return React.createElement(props.element, {
-    itemID: (typeof props.label === 'function'
-      ? '<computed>'
-      : translationKey || props.label) as string,
+  const Element = Elements[element]
 
-    style: {
-      ...getStyle(props.element),
-      ...(props.style || {}),
-    },
-    dangerouslySetInnerHTML: {
-      __html: injected,
-    },
-    ...otherProps,
-  })
+  return (
+    <Element
+      itemID={
+        (typeof label === 'function'
+          ? '<computed>'
+          : translationKey || label) as string
+      }
+      style={{
+        ...getStyle(element),
+        ...style,
+      }}
+      read-only={readOnly}
+      dangerouslySetInnerHTML={{
+        __html: injected,
+      }}
+      {...otherProps}
+    />
+  )
 }
 
-export const Option = (props: Props) => <Element {...props} element="option" />
-export const P = (props: Props) => <Element {...props} element="p" />
-export const H1 = (props: Props) => <Element {...props} element="h1" />
-export const H2 = (props: Props) => <Element {...props} element="h2" />
-export const H3 = (props: Props) => <Element {...props} element="h3" />
-export const H4 = (props: Props) => <Element {...props} element="h4" />
-export const H5 = (props: Props) => <Element {...props} element="h5" />
-export const H6 = (props: Props) => <Element {...props} element="h6" />
-export const Li = (props: Props) => <Element {...props} element="li" />
-export const button = (props: Props) => <Element {...props} element="button" />
+export const Option = (props: Props) => (
+  <Element {...props} element={'option'} />
+)
+export const P = (props: Props) => <Element {...props} element={'p'} />
+export const H1 = (props: Props) => <Element {...props} element={'h1'} />
+export const H2 = (props: Props) => <Element {...props} element={'h2'} />
+export const H3 = (props: Props) => <Element {...props} element={'h3'} />
+export const H4 = (props: Props) => <Element {...props} element={'h4'} />
+export const H5 = (props: Props) => <Element {...props} element={'h5'} />
+export const H6 = (props: Props) => <Element {...props} element={'h6'} />
+export const Li = (props: Props) => <Element {...props} element={'li'} />
+export const button = (props: Props) => (
+  <Element {...props} element={'button'} />
+)
