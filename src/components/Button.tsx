82bc32b8c4981c86d04ed7c13640d8bf3b0b3cd4
Lukas Zirngibl <lukas.e.zirngibl@gmail.com>
2021-03-11 18:28:16 +0100

    patch

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -74,6 +74,10 @@ export const Button = (props: Props) => {
       {props.icon && <Icon {...props.icon} />}
       <P
         style={{
+          color: 'inherit',
+          fontSize: 'inherit',
+          textTransform: 'inherit',
+          flexGrow: 1,
           marginLeft: props.icon === undefined ? 0 : 8,
         }}
         cssStyles={getCSSStyle('label')}
