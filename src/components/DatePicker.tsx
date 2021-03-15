ce5c32dcc5df8a126f3b6fb8859fdfe379970b67
joelmeiller <joel@oscillate.ch>
2021-03-15 12:51:00 +0100

    add animation for datepicker

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -9,7 +9,7 @@ import { TextInput } from './TextInput'
 import { format, parse } from 'date-fns'
 import { AppTheme, useAppTheme } from '../theme/theme'
 import { useInlineStyle } from '../theme/util'
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import ClickAwayListener from 'react-click-away-listener'
 import de from 'date-fns/locale/de'
 import en from 'date-fns/locale/en-GB'
@@ -34,48 +34,73 @@ const mapLanguageToLocale: { [k in Language]: Locale } = {
   [Language.IT]: it,
 }
 
+// position: absolute;
+// top: 48px;
+// right: 0px;
+// z-index: 999;
+
+// transform-origin: top center;
+//   ${({ open }: { open: boolean }) =>
+//     open
+//       ? css`
+//           opacity: 1;
+//           pointer-events: all;
+//           transform: scale(1, 1);
+//         `
+//       : css`
+//           opacity: 0;
+//           pointer-events: none;
+//           transform: scale(0, 0);
+//         `};
+//   transition: opacity 0.17s ease-out, transform 0.17s ease-out;
+
+const DatePickerIconWrapper = styled.div``
+
+const DatePickerAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform-origin: top center;
+    transform: scale(0, 0);
+  }
+  to {
+    opacity: 1;
+    transform-origin: top center;
+    transform: scale(1, 1);
+  }
+`
 const DatePickerCalendarWrapper = styled.div`
   position: absolute;
-  top: 48px;
-  right: 0px;
-  transform-origin: top center;
-  ${({ open }: { open: boolean }) =>
-    open
-      ? css`
-          opacity: 1;
-          pointer-events: all;
-          transform: scale(1, 1);
-        `
-      : css`
-          opacity: 0;
-          pointer-events: none;
-          transform: scale(0, 0);
-        `};
-  transition: opacity 0.17s ease-out, transform 0.17s ease-out;
-  z-index: 999;
+  top: 0;
+  left: 0;
+  right: 0;
 
   /* Date picker styles */
 
-  .react-datepicker__header select {
-    padding: 4px !important;
-  }
-  .react-datepicker__triangle {
+  & .react-datepicker__triangle {
     display: none !important;
   }
-  .react-datepicker-popper {
-    transform: none !important;
-  }
-  .react-datepicker-wrapper {
-    width: 1px;
-    height: 1px;
-    opacity: 0;
-    overflow: hidden;
-  }
-  .react-datepicker  {
+  & .react-datepicker-popper {
     width: 100%;
-    font-family: var(--font-family);
+    transform: none !important;
+    z-index: 999;
   }
-  .react-datepicker__day-name {
+  & .react-datepicker-wrapper {
+    display: none !important;
+  }
+
+  & .react-datepicker__header select {
+    padding: 4px !important;
+  }
+
+  & .react-datepicker  {
+    font-family: var(--font-family);
+    position: absolute;
+    top: 48px;
+    right: 0;
+    animation: ${DatePickerAnimation} 0.15s ease-out;
+  }
+  
+  & .react-datepicker__day-name {
     font-size: 16px;
     font-weight: 500;
     width: 32px;
@@ -83,29 +108,30 @@ const DatePickerCalendarWrapper = styled.div`
     padding: 9px 0 7px;
     margin: 4px;
   }
-  .react-datepicker__day {
+  & .react-datepicker__day {
     font-size: 16px;
     width: 32px;
     line-height: 16px;
     padding: 9px 0 7px;
     margin: 4px;
+    border: 1px solid transparent;
     border-radius: 0;
     :hover {
       border-radius: 0;
       background-color: rgba(249, 193, 0, 0.6);
     }
   }
-  .react-datepicker__day--today {
+  & .react-datepicker__day--today {
     background-color: var(--color-background-secondary);
+    color: var(--color-primary);
   }
-  .react-datepicker__day--selected {
-    color: var(--color-partner-primary);
+  & .react-datepicker__day--selected {
+    color: var(--color-primary);
     border: 1px solid var(--color-primary);
     background-color: var(--color-background-primary);
     font-weight: 700;
 
     :hover {
-      color: var(--color-primary);
       background-color: rgba(249, 193, 0, 0.6);
     }
   }
@@ -133,8 +159,9 @@ export const DatePicker = (props: Props) => {
 
   const locale = mapLanguageToLocale[language]
 
-  const { isMobileTouch } = useMobileTouch()
-  
+  // const { isMobileTouch } = useMobileTouch()
+  const isMobileTouch = false
+
   const [open, setOpen] = React.useState(false)
 
   const dateFormat = props.dateFormat || 'dd.MM.yyyy'
@@ -146,66 +173,83 @@ export const DatePicker = (props: Props) => {
     )
   }, [language])
 
-  console.log('OPEN', open)
   return (
     <>
       {label && <Label {...label} />}
 
-      <div {...getStyle('wrapper')}>
-        <TextInput
-          onChange={() => {}}
-          onBlur={(v) => {
-            try {
-              const value = parse(v, 'P', new Date()) as Date | 'Invalid Date'
-              onChange(value as Date)
-            } catch (err) {
-              onChange(null)
-            }
-          }}
-          inputType={isMobileTouch ? 'date' : 'text'}
-          placeholder={dateFormat.toLocaleUpperCase()}
-          value={props.value ? format(props.value, 'P', { locale }) : null}
-          dataTestId={props.dataTestId}
-        />
-        {!isMobileTouch && (
-          <>
-            <div
-              onClick={() => {
-                setOpen(!open)
+      <ClickAwayListener
+        onClickAway={() => {
+          open && setOpen(false)
+        }}
+      >
+        <div {...getStyle('wrapper')}>
+          <TextInput
+            onChange={() => {}}
+            onBlur={(v) => {
+              try {
+                const value = parse(v, 'P', new Date()) as Date | 'Invalid Date'
+                onChange(value as Date)
+              } catch (err) {
+                onChange(null)
+              }
+            }}
+            inputType={isMobileTouch ? 'date' : 'text'}
+            placeholder={dateFormat.toLocaleUpperCase()}
+            value={props.value ? format(props.value, 'P', { locale }) : null}
+            dataTestId={props.dataTestId}
+          />
+
+          <DatePickerIconWrapper
+            onClick={() => {
+              setOpen(!open)
+            }}
+            {...getStyle('iconWrapper')}
+          >
+            <div {...getStyle('hook1')} />
+            <div {...getStyle('hook2')} />
+            <Icon icon="calendar_today" size={16} />
+          </DatePickerIconWrapper>
+
+          <DatePickerCalendarWrapper>
+            <ReactDatePicker
+              locale={language}
+              open={open}
+              selected={value}
+              onChange={(v: Date) => {
+                onChange(v)
+                setOpen(false)
               }}
-              {...getStyle('iconWrapper')}
+              peekNextMonth
+              showMonthDropdown
+              showYearDropdown
+              dropdownMode="select"
+              {...props.datePickerProps}
+            />
+          </DatePickerCalendarWrapper>
+
+          {/* !isMobileTouch && (
+            <DatePickerCalendarWrapper
+              open={open}
             >
-              <div {...getStyle('hook1')} />
-              <div {...getStyle('hook2')} />
-              <Icon icon="calendar_today" size={16} />
-            </div>
-            <ClickAwayListener
-              onClickAway={() => {
-                open && setOpen(false)
-              }}
-            >
-              <DatePickerCalendarWrapper open={open}>
-                <ReactDatePicker
-                  wrapperClassName="calendar"
-                  locale={language}
-                  selected={value}
-                  onChange={(v: Date) => {
-                    console.log('ON CHANGE')
-                    onChange(v)
-                    setOpen(false)
-                  }}
-                  inline
-                  peekNextMonth
-                  showMonthDropdown
-                  showYearDropdown
-                  dropdownMode="select"
-                  {...props.datePickerProps}
-                />
-              </DatePickerCalendarWrapper>
-            </ClickAwayListener>
-          </>
-        )}
-      </div>
+              <ReactDatePicker
+                locale={language}
+                open={open}
+                selected={value}
+                onChange={(v: Date) => {
+                  onChange(v)
+                  setOpen(false)
+                }}
+                inline
+                peekNextMonth
+                showMonthDropdown
+                showYearDropdown
+                dropdownMode="select"
+                {...props.datePickerProps}
+              />
+            </DatePickerCalendarWrapper>
+              ) */}
+        </div>
+      </ClickAwayListener>
     </>
   )
 }
