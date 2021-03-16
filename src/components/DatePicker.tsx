9375280293ba612664c16ebcefaed8ac01e839d6
Lukas Zirngibl <lukas.e.zirngibl@gmail.com>
2021-03-16 11:36:23 +0100

    patch

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -176,9 +176,25 @@ export const DatePicker = (props: Props) => {
                     const dateValue = parse(v, 'P', new Date(), { locale }) as
                       | Date
                       | 'Invalid Date'
+
+                    console.log('dateValue: ', dateValue)
+                    if (dateValue == 'Invalid Date') {
+                      throw 'Invalid Date'
+                    }
+
                     onChange(dateValue as Date)
                   } catch (err) {
-                    onChange(null)
+                    const testValue = parse(
+                      v,
+                      props.dateFormat || 'yyyy-MM-dd',
+                      new Date(),
+                    )
+                    console.log(testValue)
+                    if (testValue) {
+                      onChange(testValue as Date)
+                    } else {
+                      onChange(null)
+                    }
                   }
                 }}
                 error={props.error}
