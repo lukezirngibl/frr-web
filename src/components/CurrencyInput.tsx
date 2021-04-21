7dd50ba882fea0bb07c9d2f319339abf8e041908
joelmeiller <joel@oscillate.ch>
2021-04-21 14:12:54 +0200

    make CurrencyInput work with comma and point user input

--- src/components/CurrencyInput.tsx
+++ src/components/CurrencyInput.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
+import { LabelProps } from './Label'
+import { TextInput, Props as TextInputProps } from './TextInput'
 import { TextNumberInput, Props as TextNumberProps } from './TextNumberInput'
 
 export type Props = {
-  error?: boolean
-} & TextNumberProps
+  label?: LabelProps
+  onChange: (n: string) => void
+  value: number | null | undefined
+  max?: number
+  min?: number
+} & Omit<TextInputProps, 'onChange' | 'value'>
 
-const parseAmount = (value: any): string =>
-  isNaN(value)
-    ? ''
-    : Number(value)
-        .toFixed(2)
-        .replace(/\.0{2}$/, '')
+const getValue = (v: string): string | null => {
+  const value = v.replace(',', '.')
+  return value
+}
+
+const parseAmount = (v: string): string => {
+  const value = Number(v.replace(',', '.'))
+  return isNaN(value) ? v : value.toFixed(2).replace(/\.0{2}$/, '')
+}
 
 export const CurrencyInput = (props: Props) => {
-  return <TextNumberInput {...props} parseValue={parseAmount} />
+  const { value } = props
+
+  console.log('CURRENCY VALUE', value)
+  return (
+    <TextInput
+      {...props}
+      onChange={(v) => {
+        props.onChange(getValue(v))
+      }}
+      value={!value || isNaN(value) ? undefined : `${value}`}
+      parseValue={parseAmount}
+    />
+  )
 }
