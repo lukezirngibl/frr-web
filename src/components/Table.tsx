e4dd8aa6596b478b316a2b4f61b3adfb1b7ef241
joelmeiller <joel@oscillate.ch>
2021-06-15 16:40:46 +0200

    provide some more styling options for rows incl. Amount Value

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,7 +7,7 @@ import { createStyled, useCSSStyles } from '../theme/util'
 import { Translate } from '../translation'
 import { Loading } from './Loading'
 import { P } from '../html'
-import { CSSProperties } from 'styled-components'
+import styled, { CSSProperties } from 'styled-components'
 
 const NoResultsFoundWrapper = createStyled('div')
 const LoaderWrapper = createStyled('div')
@@ -16,16 +16,29 @@ const ListWrapper = createStyled('div')
 const Header = createStyled('div')
 const HeaderOuterWrapper = createStyled('div')
 const HeaderCell = createStyled('div')
-const HeaderValue = createStyled('p')
+const HeaderValue = createStyled(styled.p`
+  text-overflow: ellipsis;
+  overflow: hidden;
+`)
 const Row = createStyled('div')
 const RowCell = createStyled('div')
-const RowValue = createStyled('p')
+const RowValue = createStyled(styled.p`
+  text-overflow: ellipsis;
+  overflow: hidden;
+`)
+const RowAmountValue = createStyled(styled.p`
+  text-align: right;
+  max-width: 90px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`)
 
 export type TableColumn<T extends {}> = {
   dataKey: keyof T
   label: string
   customRender?: (value: T[keyof T], row: T, translate: Translate) => ReactNode
   width: number
+  isAmountValue?: boolean
 }
 
 export type TableColumns<T extends {}> = Array<TableColumn<T>>
@@ -72,11 +85,10 @@ export const Table = <T extends {}>(props: Props<T>) => {
               })}
               key={c.dataKey}
             >
-              {c.customRender ? (
-                c.customRender(value, row, translate)
-              ) : (
-                <RowValue {...getCSSStyle('rowText')}>{value}</RowValue>
-              )}
+              {(c.customRender && c.customRender(value, row, translate)) ||
+                (c.isAmountValue && (
+                  <RowAmountValue {...getCSSStyle('rowText')}>{value}</RowAmountValue>
+                )) || <RowValue {...getCSSStyle('rowText')}>{value}</RowValue>}
             </RowCell>
           )
         })}
