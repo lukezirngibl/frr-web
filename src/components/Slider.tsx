f0fb16e3bcff72b2431909e4a88501201895be2f
joelmeiller <joel@oscillate.ch>
2022-05-09 15:01:08 +0200

    add data attribute to slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -142,7 +142,7 @@ export const Slider = (props: Props) => {
           {prefix && (
             <P label={prefix} localeNamespace={props.localeNamespace} {...getCSSStyles('prefix')} />
           )}
-          <ValueText {...getCSSStyles('value')}>
+          <ValueText {...getCSSStyles('value')} data-test-id="amnout" data-value={internalValue}>
             {props.isCurrency ? Formatter.format(internalValue) : internalValue}
           </ValueText>
         </Wrapper>
