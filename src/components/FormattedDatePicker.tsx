4232f76745f329297fc091fc49dd2b2930c8af54
joelmeiller <joel@oscillate.ch>
2022-08-04 10:36:03 +0200

    use maskedDatePicker for FormattedDatePicker

--- src/components/FormattedDatePicker.tsx
+++ src/components/FormattedDatePicker.tsx
@@ -1,36 +1,40 @@
 import React from 'react'
 import { parse, format, isValid } from 'date-fns'
-import { DatePicker, Props as DatePickerProps } from './DatePicker'
+import { MaskedDatePicker, Props as DatePickerProps } from './MaskedDatePicker'
 
 export type Props = {
   onChange: (value: string | null) => void
   onBlur: (value: string | null) => void
   value: string | null
   dateFormat: string
-} & Omit<DatePickerProps, 'onChange' | 'onBlur' | 'value'>
+  maskInput?: DatePickerProps['maskInput']
+} & Omit<DatePickerProps, 'onChange' | 'onBlur' | 'value' | 'maskInput'>
 
 export const FormattedDatePicker = (props: Props) => {
   const { onChange, onBlur, value, dateFormat, ...otherProps } = props
   const val: Date = props.value ? parse(props.value, props.dateFormat, new Date()) : null
 
   return (
-    <DatePicker
-      value={val as Date}
-      onChange={(value: Date) => {
+    <MaskedDatePicker
+      value={props.value}
+      onChange={(value: string) => {
         if (value !== null && isValid(value)) {
-          onChange(format(value, props.dateFormat))
+          onChange(value)
         } else {
           onChange(null)
         }
       }}
-      onBlur={(value: Date) => {
+      onBlur={(value: string) => {
         if (value !== null && isValid(value)) {
-          onBlur(format(value, props.dateFormat))
+          onBlur(value)
         } else {
           onBlur(null)
         }
       }}
       dateFormat={dateFormat}
+      maskInput={
+        props.maskInput || { alwaysShowMask: true, maskString: 'DD.MM.YYYY', mask: '00.00.0000' }
+      }
       {...otherProps}
     />
   )
