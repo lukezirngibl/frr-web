46a80882c94524373e60af0da49cc69c665f6cbd
joelmeiller <joel@oscillate.ch>
2022-08-29 09:11:36 +0200

    update Toggle component to support onFocus

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,26 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
-import { ComponentTheme, useComponentTheme, useInlineStyle } from '../theme/theme.components'
+import {
+  ComponentTheme,
+  useComponentTheme,
+  useCSSStyles,
+  useInlineStyle,
+} from '../theme/theme.components'
+import { createStyled } from '../theme/util'
 import { Label, LabelProps } from './Label'
-
-const Wrapper = styled.div`
-  transition: all ease 0.7s;
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  height: 32px;
-  width: 52px;
-  min-width: 52px;
-  padding: 2px;
-  border-radius: 16px;
-  cursor: pointer;
-`
-const Circle = styled.div`
-  transition: all ease 0.4s;
-  width: 28px;
-  height: 28px;
-  border-radius: 14px;
-`
 
 export type Props = {
   onChange: (value: boolean) => void
@@ -35,7 +22,7 @@ export type Props = {
 
 export const Toggle = (props: Props) => {
   const theme = useComponentTheme()
-  const getInlineStyle = useInlineStyle(theme, 'toggle')(props.style)
+  const getCSSStyles = useCSSStyles(theme, 'toggle')(props.style)
 
   React.useEffect(() => {
     if (props.value === null && props.defaultValue !== undefined) {
@@ -43,24 +30,49 @@ export const Toggle = (props: Props) => {
     }
   }, [])
 
+  const [isFocused, setIsFocused] = React.useState(false)
+
+  const onFocus = () => {
+    setIsFocused(true)
+  }
+
+  const onBlur = () => {
+    setIsFocused(false)
+  }
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (['ArrowRight'].includes(event.key)) {
+      setIsFocused(true)
+      props.onChange(!props.value)
+      event.preventDefault()
+    } else if (['ArrowLeft', 'Backspace'].includes(event.key)) {
+      setIsFocused(true)
+      props.onChange(!props.value)
+      event.preventDefault()
+    } else if (['Enter'].includes(event.key)) {
+      props.onChange(!props.value)
+      event.preventDefault()
+    }
+  }
+
   return (
     <>
-      {props.label && <Label {...props.label} />}
+      {props.label && <Label {...props.label} isFocused={isFocused} />}
       <Wrapper
         data-test-id={props.dataTestId}
         data-checked={!!props.value}
         onClick={() => props.onChange(!props.value)}
-        {...{
-          ...getInlineStyle('wrapper'),
-          ...(props.value ? getInlineStyle('wrapperActive') : {}),
-        }}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        onKeyDown={onKeyDown}
+        tabIndex={0}
+        {...getCSSStyles({
+          wrapper: true,
+          wrapperActive: props.value,
+          wrapperFocus: isFocused,
+        })}
       >
-        <Circle
-          {...{
-            ...getInlineStyle('circle'),
-            ...(props.value ? getInlineStyle('circleActive') : {}),
-          }}
-        >
+        <Circle {...getCSSStyles({ circle: true, circleActive: props.value })} tabIndex={-1}>
           <button
             style={{
               width: 1,
@@ -73,9 +85,29 @@ export const Toggle = (props: Props) => {
               e.preventDefault()
               props.onChange(!props.value)
             }}
+            tabIndex={-1}
           />
         </Circle>
       </Wrapper>
     </>
   )
 }
+
+const Wrapper = createStyled('div')`
+  transition: all ease 0.7s;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  height: 32px;
+  width: 52px;
+  min-width: 52px;
+  padding: 2px;
+  border-radius: 16px;
+  cursor: pointer;
+`
+const Circle = createStyled('div')`
+  transition: all ease 0.4s;
+  width: 28px;
+  height: 28px;
+  border-radius: 14px;
+`
