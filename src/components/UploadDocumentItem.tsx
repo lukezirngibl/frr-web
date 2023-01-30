3466ce42e84772620576264c824be30487fa2ff2
joelmeiller <joel@oscillate.ch>
2023-01-30 21:43:35 +0100

    adjust styling of rejected files

---
+++ src/components/UploadDocumentItem.tsx
@@ -0,0 +1,71 @@
+import HighlightOffIcon from '@material-ui/icons/HighlightOff'
+import React, { useEffect, useReducer, useState } from 'react'
+import { FileRejection, useDropzone } from 'react-dropzone'
+import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
+import { P } from '../html'
+import { ComponentTheme, useComponentTheme, useCSSStyles } from '../theme/theme.components'
+import { createStyled } from '../theme/util'
+import { LocaleNamespace } from '../translation'
+
+export const formatFileSize = (size: number) => {
+  const formattedSize: number = size / 1000
+  if (formattedSize > 1000) return `${(formattedSize / 1000).toFixed(2)} MB`
+
+  return `${formattedSize.toFixed(2)} KB`
+}
+
+type UploadDocumentItemProps = {
+  file: File
+  fileLabel?: string
+  maxFilesToUpload: number
+  maxFileSize: number
+  onRemove: () => void
+  style?: Partial<ComponentTheme['uploadDropzone']>
+}
+export const UploadDocumentItem = (props: UploadDocumentItemProps) => {
+  const theme = useComponentTheme()
+  const getCSSStyle = useCSSStyles(theme, 'uploadDropzone')(props.style)
+
+  return (
+    <ListItem
+      key={props.file.name}
+      {...getCSSStyle({
+        listItem: true,
+        listSingleItem: props.maxFilesToUpload === 1,
+      })}
+    >
+      {props.file.name.endsWith('.png' || '.jpeg' || '.svg') && (
+        <ItemIcon
+          src={URL.createObjectURL(props.file)}
+          alt={props.file.name}
+          {...getCSSStyle('imageItem')}
+        />
+      )}
+      <P
+        isLabelTranslated
+        label={props.fileLabel || `${props.file.name} - ${formatFileSize(props.file.size)}`}
+        data={{
+          fileName: props.file.name,
+          maxFileSize: formatFileSize(props.maxFileSize),
+          fileSize: formatFileSize(props.file.size),
+        }}
+        {...getCSSStyle({ acceptedFileItem: true })}
+      />
+      <RemoveItemIcon {...getCSSStyle('removeItemIcon')}>
+        <HighlightOffIcon className="remove-icon" onClick={props.onRemove} />
+      </RemoveItemIcon>
+    </ListItem>
+  )
+}
+
+const ListItem = createStyled('div')
+
+const ItemIcon = createStyled('img')
+
+const RemoveItemIcon = createStyled(styled.div`
+  svg.remove-icon {
+    width: 100%;
+    height: 100%;
+  }
+`)
