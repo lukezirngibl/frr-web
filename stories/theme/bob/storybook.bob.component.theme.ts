d3b719f7c277f97c9f9184383775c0e27cda4cd7
joelmeiller <joel@oscillate.ch>
2023-03-01 14:37:47 +0100

    revert styles

--- stories/theme/bob/storybook.bob.component.theme.ts
+++ stories/theme/bob/storybook.bob.component.theme.ts
@@ -74,7 +74,6 @@ export const componentTheme: Partial<ComponentThemeConfig> = {
     },
     itemIcon: {
       marginRight: 4,
-      marginBottom: 2,
       width: 18,
     },
     iconAllowed: {
@@ -111,7 +110,7 @@ export const componentTheme: Partial<ComponentThemeConfig> = {
   popoverDropdown: {
     icon: {
       marginLeft: 8,
-      marginBottom: 2,
+      marginBottom: -2,
     },
     button: {
       display: 'flex',
