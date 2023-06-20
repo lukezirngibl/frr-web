345c49d01f92fdbd535e9360616351d7cffae28b
joelmeiller <joel@oscillate.ch>
2023-06-20 10:40:00 +0200

    optimize style injection for icons

--- src/icons/utils/toIcon.tsx
+++ src/icons/utils/toIcon.tsx
@@ -1,20 +1,27 @@
 import { ReactNode } from 'react'
 import { createStyled } from '../../theme/util'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { CSSProperties } from '../../theme/configure.theme'
 
 type IconProps = {
   dataTestId?: string
   onClick?: () => void
   size?: number
-  cssStyles?: { cssStyles: string; dataThemeId: string }
+  dataThemeId?: string
+  cssStyles?: string
+  cssStylesSVG?: string
+  style?: CSSProperties
 }
 export const toIcon = (SVG: ReactNode) => (props: IconProps) =>
   (
     <IconDiv
+      cssStyles={props.cssStyles}
+      cssStylesSVG={props.cssStylesSVG}
       data-test-id={props.dataTestId}
+      dataThemeId={props.dataThemeId}
       onClick={props.onClick}
       size={props.size}
-      {...props.cssStyles}
+      style={props.style}
     >
       {SVG}
     </IconDiv>
@@ -24,4 +31,8 @@ const IconDiv = createStyled(styled.div<IconProps>`
   width: ${(props) => props.size || 24}px;
   height: ${(props) => props.size || 24}px;
   cursor: ${(props) => (props.onClick ? 'pointer' : 'default')};
+
+  svg {
+    ${(props) => css`${props.cssStylesSVG}`}
+  }
 `)
