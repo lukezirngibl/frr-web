cb9414bca44cf9f69069c3112f7329d7596c0cce
joelmeiller <joel@oscillate.ch>
2023-09-21 13:00:55 +0200

    fix type build issue with React for icons in storybook

--- src/icons/new/MdClose.tsx
+++ src/icons/new/MdClose.tsx
@@ -1,6 +1,6 @@
-import { SVGProps } from 'react'
+import React from 'react'
 
-export const MdClose: React.FC<SVGProps<SVGSVGElement>> = (props) => {
+export const MdClose = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" {...props}>
       <path fill="none" d="M0 0h24v24H0z"></path>
