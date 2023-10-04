be01cd4e2ff4a80e3cac7bbd53eda0f7cf9599b3
joelmeiller <joel@oscillate.ch>
2023-10-04 12:55:08 +0200

    use @emotion for MUI styling

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,10 +21,10 @@ module.exports = {
     options: {},
   },
   webpackFinal: async (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@mui/styled-engine': '@mui/styled-engine-sc',
-    }
+    // config.resolve.alias = {
+    //   ...config.resolve.alias,
+    //   '@mui/styled-engine': '@mui/styled-engine-sc',
+    // }
     config.resolve.fallback = {
       ...config.resolve.fallback,
       buffer: require.resolve('buffer'),
