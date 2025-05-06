27a271e94a1d2a52eed9cd7f9fb0967e3ff50568
joelmeiller <joel@oscillate.ch>
2025-05-06 15:06:34 +0200

    add dataTestId to errorButton in label component

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -26,6 +26,7 @@ export type LabelProps = {
   errorLabel?: string | string[]
   errorLabelData?: Record<string, string>
   errorButton?: {
+    dataTestId?: string
     errorLabel: string
     onClick: () => void
     label: string
@@ -144,6 +145,7 @@ export const Label = (props: LabelProps) => {
 
             {props.errorButton && errorLabels.includes(props.errorButton.errorLabel) && (
               <Link
+                data-test-id={props.errorButton.errorLabel}
                 icon={{ type: 'edit', style: getCSSStyles('errorButtonIcon') }}
                 onClick={props.errorButton.onClick}
                 label={props.errorButton.label}
