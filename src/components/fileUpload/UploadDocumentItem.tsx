19bb2e697148b9ec5cdb6a159734e831387db559
joelmeiller <joel@oscillate.ch>
2025-07-03 19:42:01 +0200

    adjust UploadDocumentItem labels

--- src/components/fileUpload/UploadDocumentItem.tsx
+++ src/components/fileUpload/UploadDocumentItem.tsx
@@ -5,6 +5,7 @@ import { ComponentTheme, useCSSStyles, useComponentTheme } from '../../theme/the
 import { createStyled } from '../../theme/util'
 import { MdOutlineCancel } from '../../icons/new/MdOutlineCancel'
 import { BsFiletypePdf } from '../../icons/new/BsFiletypePdf'
+import { AiOutlineCheck } from '../../icons/new/AiOutlineCheck'
 
 export const formatFileSize = (size: number) => {
   const formattedSize: number = size / 1000
@@ -13,7 +14,13 @@ export const formatFileSize = (size: number) => {
   return `${formattedSize.toFixed(2)} KB`
 }
 
-export type UploadedFile = { name: string; format?: string; previewUri?: string; size?: number }
+export type UploadedFile = {
+  format?: string
+  isFileHidden?: boolean
+  name: string
+  previewUri?: string
+  size?: number
+}
 
 type UploadDocumentItemProps = {
   file: File | UploadedFile
@@ -27,51 +34,75 @@ export const UploadDocumentItem = (props: UploadDocumentItemProps) => {
   const theme = useComponentTheme()
   const getCSSStyle = useCSSStyles(theme, 'uploadDropzone')(props.style)
 
+  const fileLabel =
+    props.fileLabel ||
+    (props.file.size && `${props.file.name} - ${formatFileSize(props.file.size || 0)}`) ||
+    props.file.name
+
+  const isFileHidden = 'isFileHidden' in props.file ? props.file.isFileHidden : false
+
   return (
     <Div
-      key={props.file.name}
       {...getCSSStyle({
         listItem: true,
         listSingleItem: props.maxFilesToUpload === 1,
       })}
+      key={props.file.name}
+      readOnly={isFileHidden}
     >
-      <PreviewImageWrapper {...getCSSStyle('imageItem')}>
-        {props.file instanceof File
-          ? props.file.name.endsWith('.png') ||
-            props.file.name.endsWith('.jpeg') ||
-            (props.file.name.endsWith('.svg') && (
-              <Img
-                src={URL.createObjectURL(props.file)}
-                alt={props.file.name}
-                {...getCSSStyle('imageItem')}
-              />
-            )) ||
-            (props.file.type === 'application/pdf' && (
-              <div className="pdf-icon-wrapper">
-                <BsFiletypePdf className="pdf-icon" />
-              </div>
-            ))
-          : (props.file as UploadedFile).previewUri && (
-              <Img
-                src={(props.file as UploadedFile).previewUri}
-                alt={props.file.name}
-                {...getCSSStyle('imageItem')}
-              />
-            )}
+      <PreviewImageWrapper {...getCSSStyle('imageItem')} readOnly={isFileHidden}>
+        {isFileHidden ? (
+          <div className="icon-wrapper">
+            <AiOutlineCheck className="check-icon" />
+          </div>
+        ) : props.file instanceof File ? (
+          props.file.name.endsWith('.png') ||
+          props.file.name.endsWith('.jpeg') ||
+          (props.file.name.endsWith('.svg') && (
+            <Img
+              src={URL.createObjectURL(props.file)}
+              alt={props.file.name}
+              {...getCSSStyle('imageItem')}
+            />
+          )) ||
+          (props.file.type === 'application/pdf' && (
+            <div className="icon-wrapper">
+              <BsFiletypePdf className="pdf-icon" />
+            </div>
+          ))
+        ) : (
+          (props.file.previewUri && (
+            <Img
+              src={(props.file as UploadedFile).previewUri}
+              alt={props.file.name}
+              {...getCSSStyle('imageItem')}
+            />
+          )) ||
+          (props.file.format === 'pdf' && (
+            <div className="icon-wrapper">
+              <BsFiletypePdf className="pdf-icon" />
+            </div>
+          ))
+        )}
       </PreviewImageWrapper>
+
       <P
         isLabelTranslated
-        label={props.fileLabel || `${props.file.name} - ${formatFileSize(props.file.size)}`}
+        readOnly={isFileHidden}
+        label={props.fileLabel || fileLabel}
         data={{
           fileName: props.file.name,
-          maxFileSize: formatFileSize(props.maxFileSize),
           fileSize: formatFileSize(props.file.size),
+          maxFileSize: formatFileSize(props.maxFileSize),
         }}
         {...getCSSStyle({ acceptedFileItem: true })}
       />
-      <RemoveItemIcon {...getCSSStyle('removeItemIcon')}>
-        <MdOutlineCancel className="remove-icon" onClick={props.onRemove} width={16} />
-      </RemoveItemIcon>
+
+      {!isFileHidden && (
+        <RemoveItemIcon {...getCSSStyle('removeItemIcon')}>
+          <MdOutlineCancel className="remove-icon" onClick={props.onRemove} width={16} />
+        </RemoveItemIcon>
+      )}
     </Div>
   )
 }
@@ -81,17 +112,23 @@ const PreviewImageWrapper = createStyled(styled.div`
     width: 100%;
   }
 
-  .pdf-icon-wrapper {
+  .icon-wrapper {
     width: 100%;
     height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
+    color: currentColor;
   }
 
   svg.pdf-icon {
     width: 28px;
     height: 28px;
+  }
+
+  svg.check-icon {
+    width: 24px;
+    height: 24px;
   }
 `)
 
